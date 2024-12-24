5361891ee05f24355b0deb6aac686de04831458b
duong.minh.truong <duong.minh.truong@vinicorp.com.vn>
2024-12-24 16:55:17 +0700

    switch to postgresql

--- go.mod
+++ go.mod
@@ -29,6 +29,10 @@ require (
 	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
 	github.com/googleapis/gax-go/v2 v2.14.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
@@ -50,6 +54,7 @@ require (
 	golang.org/x/crypto v0.30.0 // indirect
 	golang.org/x/net v0.32.0 // indirect
 	golang.org/x/oauth2 v0.24.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/api v0.211.0 // indirect
@@ -60,6 +65,7 @@ require (
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.5.7 // indirect
+	gorm.io/driver/postgres v1.5.11 // indirect
 	gorm.io/driver/sqlite v1.5.6 // indirect
 	gorm.io/gorm v1.25.11 // indirect
 )
--- src/database/database.go
+++ src/database/database.go
@@ -1,3 +1,75 @@
+// package database
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+
+// 	models "caapp-server/src/models/db_models"
+// 	rcdbmodels "caapp-server/src/models/db_models/rc_db_models"
+
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/driver/postgres"
+// 	"gorm.io/gorm"
+// )
+
+// var DB *gorm.DB
+
+// func Connect() {
+// 	// err := godotenv.Load(".env")
+// 	// if err != nil {
+// 	// 	log.Fatal("Error loading .env file")
+// 	// }
+
+// 	// env := os.Getenv("APP_ENV")
+// 	// envFile := fmt.Sprintf(".env.%s", env)
+// 	// err := godotenv.Load(envFile)
+// 	// if err != nil {
+// 	// 	log.Fatalf("Error loading %s file: %v", envFile, err)
+// 	// }
+
+// 	dbHost := os.Getenv("DB_HOST")
+// 	dbPort := os.Getenv("DB_PORT")
+// 	dbUser := os.Getenv("DB_USER")
+// 	dbPassword := os.Getenv("DB_PASSWORD")
+// 	dbName := os.Getenv("DB_NAME")
+
+// 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+// 	var dbErr error
+// 	DB, dbErr = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	if dbErr != nil {
+// 		log.Fatal("Failed to connect to database:", dbErr)
+// 	}
+
+// 	// Using the database 'caapp'
+// 	useDB := fmt.Sprintf("USE %s", dbName)
+// 	if dbErr := DB.Exec(useDB).Error; dbErr != nil {
+// 		log.Fatal("Failed to select database:", dbErr)
+// 	}
+
+// 	// Automatically migrate schema
+// 	DB.AutoMigrate(&models.User{})
+// 	DB.AutoMigrate(&models.Message{})
+// 	DB.AutoMigrate(&models.Channel{})
+// 	DB.AutoMigrate(&models.ChannelMember{})
+// 	DB.AutoMigrate(&models.Media{})
+// 	DB.AutoMigrate(&models.Friend{})
+// 	DB.AutoMigrate(&models.FriendRequest{})
+// 	DB.AutoMigrate(&models.EmailValidateCode{})
+// 	DB.AutoMigrate(&models.ForgotPasswordValidateCode{})
+
+// 	// rc module
+// 	DB.AutoMigrate(&rcdbmodels.MQueue{})
+// 	DB.AutoMigrate(&rcdbmodels.FQueue{})
+// 	DB.AutoMigrate(&rcdbmodels.RCChannel{})
+// 	DB.AutoMigrate(&rcdbmodels.RCChannelMember{})
+// 	DB.AutoMigrate(&rcdbmodels.RCMessage{})
+// 	DB.AutoMigrate(&rcdbmodels.GeneralQueueUser{})
+// 	DB.AutoMigrate(&rcdbmodels.RCMedia{})
+
+//		fmt.Println("Connected to", dbName, "database...")
+//	}
 package database
 
 import (
@@ -8,64 +80,49 @@ import (
 	models "caapp-server/src/models/db_models"
 	rcdbmodels "caapp-server/src/models/db_models/rc_db_models"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func Connect() {
-	// err := godotenv.Load(".env")
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
-
-	// env := os.Getenv("APP_ENV")
-	// envFile := fmt.Sprintf(".env.%s", env)
-	// err := godotenv.Load(envFile)
-	// if err != nil {
-	// 	log.Fatalf("Error loading %s file: %v", envFile, err)
-	// }
-
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
+	sslMode := os.Getenv("DB_SSLMODE")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", dbHost, dbPort, dbUser, dbPassword, dbName, sslMode)
 
 	var dbErr error
-	DB, dbErr = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB, dbErr = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if dbErr != nil {
 		log.Fatal("Failed to connect to database:", dbErr)
 	}
 
-	// Using the database 'caapp'
-	useDB := fmt.Sprintf("USE %s", dbName)
-	if dbErr := DB.Exec(useDB).Error; dbErr != nil {
-		log.Fatal("Failed to select database:", dbErr)
+	err := DB.AutoMigrate(
+		&models.User{},
+		&models.Message{},
+		&models.Channel{},
+		&models.ChannelMember{},
+		&models.Media{},
+		&models.Friend{},
+		&models.FriendRequest{},
+		&models.EmailValidateCode{},
+		&models.ForgotPasswordValidateCode{},
+		&rcdbmodels.MQueue{},
+		&rcdbmodels.FQueue{},
+		&rcdbmodels.RCChannel{},
+		&rcdbmodels.RCChannelMember{},
+		&rcdbmodels.RCMessage{},
+		&rcdbmodels.GeneralQueueUser{},
+		&rcdbmodels.RCMedia{},
+	)
+	if err != nil {
+		log.Fatal("Failed to migrate database schema:", err)
 	}
-
-	// Automatically migrate schema
-	DB.AutoMigrate(&models.User{})
-	DB.AutoMigrate(&models.Message{})
-	DB.AutoMigrate(&models.Channel{})
-	DB.AutoMigrate(&models.ChannelMember{})
-	DB.AutoMigrate(&models.Media{})
-	DB.AutoMigrate(&models.Friend{})
-	DB.AutoMigrate(&models.FriendRequest{})
-	DB.AutoMigrate(&models.EmailValidateCode{})
-	DB.AutoMigrate(&models.ForgotPasswordValidateCode{})
-
-	// rc module
-	DB.AutoMigrate(&rcdbmodels.MQueue{})
-	DB.AutoMigrate(&rcdbmodels.FQueue{})
-	DB.AutoMigrate(&rcdbmodels.RCChannel{})
-	DB.AutoMigrate(&rcdbmodels.RCChannelMember{})
-	DB.AutoMigrate(&rcdbmodels.RCMessage{})
-	DB.AutoMigrate(&rcdbmodels.GeneralQueueUser{})
-	DB.AutoMigrate(&rcdbmodels.RCMedia{})
 
 	fmt.Println("Connected to", dbName, "database...")
 }
